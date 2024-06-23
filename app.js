acae5010f29e0656c4754a3c5ad54984cc52c4e6
unknown <kendrick_sin@hotmail.com>
2024-06-23 22:49:48 +0800

    api works

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const router = require( './routes/server.js'); 
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use('/', router); 
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
